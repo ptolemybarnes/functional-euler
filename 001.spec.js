2b5df5df29d967a43ffacd056d5db11f97265a58
Ptolemy <ptolemybarnes@gmail.com>
2015-10-26 21:43:13 +0000

    Solves 001

--- 001.spec.js
+++ 001.spec.js
@@ -1,10 +1,24 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 //
+var R = require('ramda');
 
+
+var selectMultiplesOfThree = function(value) { return value % 3 == 0 };
+var selectMultiplesOfFive  = function(value) { return value % 5 == 0 };
+
+var filterWithTwo = function(firstCondition, secondCondition) {
+  return R.filter(function(value) {
+    return (firstCondition(value) || secondCondition(value))
+  })
+}
+
+var selectMultiplesOfThreeOrFive = filterWithTwo(selectMultiplesOfThree, selectMultiplesOfFive);
+var adder                        = function(sum, value) { return sum + value };
+var findSumOfMultiplesBelow      = R.compose(R.reduce(adder, 0), filterWithTwo(selectMultiplesOfThree, selectMultiplesOfFive));
 
 describe('001', function() { 
   it('finds the sum of all multiples of 3 or 5 below 1000', function() {
-    expect(findSumOfMultiplesBelow(1000)).toEqual(233168)
+    expect(findSumOfMultiplesBelow(R.range(1, 1000))).toEqual(233168)
   });
 });
